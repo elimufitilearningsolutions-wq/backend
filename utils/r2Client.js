3dc32b885a05df8903d4f266da31bc65bbf2e812
selene <selineecl25@gmail.com>
2026-01-05 08:36:38 +0300

    3rd commit

--- utils/r2Client.js
+++ utils/r2Client.js
@@ -7,9 +7,10 @@ const r2Client = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: String(process.env.R2_ACCESS_KEY_ID),
+    secretAccessKey: String(process.env.R2_SECRET_ACCESS_KEY),
   },
+  forcePathStyle: true, // 🔑 REQUIRED for Cloudflare R2
 });
 
 export default r2Client;
