a077d735debab5570cdf3b9825805bfcd5586ca2
selene <selineecl25@gmail.com>
2026-02-18 13:26:24 +0300

    100mb

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.options("*", cors(corsOptions));
    BODY PARSERS
 ======================= */
 app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 
 /* =======================
